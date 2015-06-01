b1057fea4f66d7e37d3eb464cc5fa1fb1afed478
jimtoepel <jim.toepel@gmail.com>
2015-05-31 19:38:27 -0700

    SUPER INITS

---
+++ Appliances/BNROwnedAppliance.h
@@ -0,0 +1,19 @@
+//
+//  BNROwnedAppliance.h
+//  Appliances
+//
+//  Created by Jim Toepel on 5/31/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import "BNRAppliance.h"
+
+@interface BNROwnedAppliance : BNRAppliance
+@property (readonly) NSSet *ownerNames;
+- (instancetype)initWithProductName:(NSString *)pn
+                     firstOwnerName:(NSString *)n;
+- (void)addOwnerName:(NSString *)n;
+- (void)removeOwnerName:(NSString *)n;
+
+
+@end
---
+++ Appliances/BNROwnedAppliance.m
@@ -0,0 +1,53 @@
+//
+//  BNROwnedAppliance.m
+//  Appliances
+//
+//  Created by Jim Toepel on 5/31/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import "BNROwnedAppliance.h"
+
+@interface BNROwnedAppliance ()
+{
+    NSMutableSet *_ownerNames;
+}
+@end
+
+@implementation BNROwnedAppliance
+
+- (instancetype)initWithProductName:(NSString *)pn
+                     firstOwnerName:(NSString *)n
+{
+    // Call the superclass's initializer
+    if (self = [super initWithProductName:pn])
+        
+        // Create a set to hold owner's names
+        _ownerNames = [[NSMutableSet alloc] init];
+    
+    // Is the first owner name non0nil?
+    if (n) {
+        [_ownerNames addObject:n];
+    }
+    // return a pointer to new objects
+    return self;
+}
+
+- (void)addOwnerName:(NSString *)n
+{
+    [_ownerNames addObject:n];
+}
+
+- (void)removeOwnerName:(NSString *)n
+{
+    [_ownerNames removeObject:n];
+}
+
+-(NSSet *)ownerNames
+{
+    return [_ownerNames copy];
+}
+
+
+
+@end
